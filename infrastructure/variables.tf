532de20671f8330f25efeb263c0dd7c8bfde48e1
im-devendhar <devendharyadav781@gmail.com>
2026-03-12 20:04:35 +0530

    edited infrastructure

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,23 +1,23 @@
-var "region" {
+variable "region" {
     default = "us-east-1"
 }
 
-var "vpc_cidr" {
+variable "vpc_cidr" {
     default = "10.0.0.0/16"
 }
 
-var "public_subnet_1_cidr"{
+variable "public_subnet_1_cidr"{
     default = "10.0.1.0/24"
 }
 
-var "public_subnet_2_cidr"{
+variable "public_subnet_2_cidr"{
     default = "10.0.2.0/24"
 }
 
-var "private_subnet_1_cidr"{
+variable "private_subnet_1_cidr"{
     default = "10.0.3.0/24"
 }
 
-var "private_subnet_2_cidr"{
+variable "private_subnet_2_cidr"{
     default = "10.0.4.0/24"
 }
