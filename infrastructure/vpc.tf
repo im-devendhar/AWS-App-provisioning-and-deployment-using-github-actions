327c1573bdcd89eaa0c348a7d58e63bf5cdb9fd2
im-devendhar <devendharyadav781@gmail.com>
2026-03-13 10:11:23 +0530

    Update vpc.tf

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -1,111 +1,103 @@
+# vpc.tf
 resource "aws_vpc" "main" {
-    cidr_block = var.vpc_cidr
-    enable_dns_support = true
-    enable_dns_hostname = true
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
-    tags = {
-        Name = "main_vpc"
-    }
+  tags = { Name = "main_vpc" }
 }
 
-resource "aws_subnet" "public_subnet_1"{
-    vpc_id = aws_vpc.main.id
-    cidr_block = var.public_subnet_1_cidr
-    availability_zone = "${var.region}a"
-
-    tags = {
-        Name = "public_subnet_1"
-    }
+# Public subnets (note: map_public_ip_on_launch = true)
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_1_cidr
+  availability_zone       = "${var.region}a"
+  map_public_ip_on_launch = true
+  tags = { Name = "public_subnet_1" }
 }
 
-resource "aws_subnet" "public_subnet_2"{
-    vpc_id = aws_vpc.main.id
-    cidr_block = var.public_subnet_2_cidr
-    availability_zone = "${var.region}b"
-
-    tags = {
-        Name = "public_subnet_2"
-    }
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_2_cidr
+  availability_zone       = "${var.region}b"
+  map_public_ip_on_launch = true
+  tags = { Name = "public_subnet_2" }
 }
 
-resource "aws_subnet" "private_subnet_1"{
-    vpc_id = aws_vpc.main.id
-    cidr_block = var.private_subnet_1_cidr
-    availability_zone = "${var.region}a"
-
-    tags = {
-        Name = "private_subnet_1"
-    }
+# Private subnets (place in a and b)
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_1_cidr
+  availability_zone = "${var.region}a"
+  tags = { Name = "private_subnet_1" }
 }
 
-resource "aws_subnet" "private_subnet_2"{
-    vpc_id = aws_vpc.main.id
-    cidr_block = var.private_subnet_2_cidr
-    availability_zone = "${var.region}a"
-
-    tags = {
-        Name = "private_subnet_2"
-    }
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_2_cidr
+  availability_zone = "${var.region}b"
+  tags = { Name = "private_subnet_2" }
 }
 
-
-resource "aws_internet_gateway" "igw"{
-    vpc_id = aws_vpc.main.id
-
-    tags = {
-        Name = "igw"
-    }
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = { Name = "igw" }
 }
 
-resource "aws_route_table" "public_rt"{
-    vpc_id = aws_vpc.main.id
-
-    tags = {
-        Name = "public_rt"
-    }
+# Public route table -> IGW
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+  tags = { Name = "public_rt" }
 }
 
-resource "aws_route" "public_network"{
-    route_table_id   = aws_route.public_rt.id
-    destination_cidr = "0.0.0.0/0"
-    gateway_id       = aws_internet_gateway.igw.id
+resource "aws_route" "public_default" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
 }
 
-resource "aws_route_table_association" "public_assoc_1"{
-    subnet_id      = aws_subnet.private_subnet_1.id
-    route_table_id = aws_route_table.public_rt.id 
+# Associate public subnets to public RT
+resource "aws_route_table_association" "public_assoc_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id   
+  route_table_id = aws_route_table.public_rt.id
 }
 
-resource "aws_route_table_association" "public_assoc_2"{
-    subnet_id      = aws_subnet.private_subnet_2.id
-    route_table_id = aws_route_table.public_rt.id 
+resource "aws_route_table_association" "public_assoc_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id   
+  route_table_id = aws_route_table.public_rt.id
 }
 
-resource "aws_eip" "nat_eip"{
-    domain = "vpc"
+# NAT GW in public subnet
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
 }
 
-resource "aws_nat_gateway" "ngw"{
-    allocation_id = aws_eip.nat_eip.id
-    subnet_id     = aws_subnet.public_subnet_1.id 
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+  depends_on    = [aws_internet_gateway.igw]      
+  tags = { Name = "nat_gw" }
 }
 
-resource "aws_route_table" "private_rt"{
-    vpc_id = aws_vpc.main.id
+# Private RT -> NAT GW
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+  tags = { Name = "private_rt" }
 }
 
-resource "aws_route" "private_network"{
-    route_table_id   = aws_route_table.private_rt.id
-    destination_cidr = "0.0.0.0/0"
-    nat_gateway_id   = aws_nat_gateway.ngw.id
+resource "aws_route" "private_default" {
+  route_table_id         = aws_route_table.private_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.ngw.id
 }
 
-resource "aws_route_table_association" "private_assoc1"{
-    subnet_id      = aws_subnet.private_subnet_1.id
-    route_table_id = aws_route_table.private_rt.id
+# Associate private subnets to private RT
+resource "aws_route_table_association" "private_assoc1" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_rt.id
 }
 
-resource "aws_route_table_association" "private_assoc2"{
-    subnet_id      = aws_subnet.private_subnet_2.id
-    route_table_id = aws_route_table.private_rt.id
+resource "aws_route_table_association" "private_assoc2" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_rt.id
 }
